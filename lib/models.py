fc78451b7cc0e0e10f3f16f7ec8f59d1bc616153
mararo-ops <danielmararo1@gmail.com>
2023-12-14 09:51:26 +0300

    import messages

---
+++ lib/models.py
@@ -0,0 +1,3 @@
+from sqlalchemy import Column, Integer, String, ForeignKey,create_engine
+from sqlalchemy.orm import relationship,sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
