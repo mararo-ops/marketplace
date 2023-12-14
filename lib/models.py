0458f8a78e5df96fad3a9739bde2314638fd7d36
mararo-ops <danielmararo1@gmail.com>
2023-12-14 09:55:11 +0300

    creating the tables

--- lib/models.py
+++ lib/models.py
@@ -13,3 +13,16 @@ session = Session()
 
 #base class for allthe classes
 Base =declarative_base()
+
+#creating the tables
+class Farmer(Base):
+    __tablename__='farmers'
+    id=Column(Integer, primary_key=True)
+    name=Column(String,nullable=False)
+    email = Column(String(255), nullable=True)
+    phone_number = Column(String(20), nullable=True)
+    city = Column(String(100), nullable=True)
+
+    #relationships 
+    produce =relationship('Produce',back_populates='farmer') #one to many relationship between farmer and produce
+    orders = relationship('Order',back_populates='farmer') #a farmer can have many orders
