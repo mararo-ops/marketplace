706166c7841f7cb90d86bc1e494f4a5a72471c3f
mararo-ops <danielmararo1@gmail.com>
2023-12-14 10:27:45 +0300

    creating the engine

--- lib/cli.py
+++ lib/cli.py
@@ -2,3 +2,11 @@ import click
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Farmer,Produce,Order,Consumer
+
+# define the database connection 
+DATABASE_URI = 'sqlite:///farmers.db' #path to the database
+
+engine = create_engine(DATABASE_URI,echo=False)
+ #creating the engine
+Session = sessionmaker(bind=engine) #creating a session
+session = Session()
